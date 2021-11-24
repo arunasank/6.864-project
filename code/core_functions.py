ef8cc798b7f27867c00d141afb1f6dd7523d323e
peterdeweirdt <petedeweirdt@gmail.com>
2021-11-23 19:26:48 -0500

    code (w2v): add word2vec featurization

--- code/core_functions.py
+++ code/core_functions.py
@@ -4,10 +4,12 @@
 from sklearn import metrics
 
 
-def evaluate_model(predictions, observations, scaling, name, eval_metrics=None):
+def evaluate_model(binary_preds, continuous_preds, observations, scaling, name,
+                   binary_metrics=None, continuous_metrics=None):
     """Evaluate model predictions
 
-    :param predictions: array of binary model predictions
+    :param binary_preds: array of binary model predictions
+    :param continuous_preds: array of continuous model predictions e.g. probabilities
     :param observations: array of observed binary values
     :param scaling: array of scaling factor for each observation
     :param name: name of model, to be output with performance_dict
@@ -15,15 +17,21 @@ def evaluate_model(predictions, observations, scaling, name, eval_metrics=None):
     function will use accuracy, f1, precision, recall, and roc-auc
     :return: dict of model performance
     """
-    if eval_metrics is None:
-        eval_metrics = {'accuracy': metrics.accuracy_score,
-                        'f1': metrics.f1_score,
-                        'precision': metrics.precision_score,
-                        'recall': metrics.recall_score,
-                        'roc-auc': metrics.roc_auc_score}
-        performance_dict = {}
-        for metric_name, metric in eval_metrics.items():
-            performance_dict[metric_name] = metric(observations, predictions,
-                                                   sample_weight=scaling)
-        performance_dict['name'] = name
-        return performance_dict
+    if binary_metrics is None:
+        binary_metrics = {'accuracy': metrics.accuracy_score,
+                          'f1': metrics.f1_score,
+                          'precision': metrics.precision_score,
+                          'recall': metrics.recall_score}
+    if continuous_metrics is None:
+        continuous_metrics = {'roc-auc': metrics.roc_auc_score,
+                              'avg_precision': metrics.average_precision_score}
+    performance_dict = {}
+    for metric_name, metric in binary_metrics.items():
+        performance_dict[metric_name] = metric(observations, binary_preds,
+                                               sample_weight=scaling)
+    for metric_name, metric in continuous_metrics.items():
+        performance_dict[metric_name] = metric(observations, continuous_preds,
+                                               sample_weight=scaling)
+
+    performance_dict['name'] = name
+    return performance_dict
